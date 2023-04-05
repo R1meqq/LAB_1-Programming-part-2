165c32f9e798bd0fea07cdae1b7b17f7c4ad5484
R1meq <115041231+R1meq@users.noreply.github.com>
2023-04-05 17:17:58 +0300

    updated

--- src/ua/lviv/iot/algo/part1/ShipApp/Ship.java
+++ src/ua/lviv/iot/algo/part1/ShipApp/Ship.java
@@ -1,64 +1,23 @@
 package ua.lviv.iot.algo.part1.ShipApp;
 import lombok.*;
 
-@Getter
+
 @Setter
-@AllArgsConstructor
+@Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
-public class Ship {
-    private double id = 10.4;
-    private String name;
-    private String captain;
-    private String currentPort;
-    private double maxSpeed;
-    private double maxCapacity;
-    private double currentLoad;
-    private double currentSpeed;
-    private static Ship instance;
-
-    public String dock(String port) {
-        return currentPort = port;
-    }
-
-    public void setSpeed(double speed) {
-        if (speed >= 0.0)
-            currentSpeed = speed;
-    }
-
-    public void load(double weight) {
-        if (weight + currentLoad <= maxCapacity)
-            currentLoad += weight;
-        else {
-            currentLoad = maxCapacity;
-        }
-    }
-
-    public void unload(double weight) {
-        if (currentLoad - weight >= 0.0)
-            currentLoad -= weight;
-        else {
-            currentLoad = 0.0;
-        }
-    }
-
-    public static Ship getInstance() {
-        if (instance == null)
-            instance = new Ship();
-
-        return instance;
-    }
-
-    public static void main(String[] args) {
-        Ship ship1 = new Ship();
-        Ship ship2 = new Ship(4.2, "Meteora", "Mike Shinoda",
-                "Fort Minor", 18.2, 4.5, 3.1, 6.7);
-        Ship[] ships = {ship1, ship2, getInstance(), getInstance()};
-        ships[1].load(2.2);
-        ships[1].unload(5.2);
-
-        for (Ship ship : ships) {
-            System.out.println(ship.toString());
-        }
-    }
+public abstract class Ship {
+    protected double id = 10.4;
+    protected String name;
+    protected String captain;
+    protected String currentPort;
+    protected double maxSpeed;
+    protected double maxCapacity;
+    protected double currentLoad;
+    protected double currentSpeed;
+    protected int crewCount;
+    protected int supportStaff;
+    public abstract int getTotalPeopleCount();
+    public abstract double calculateLoadTime();
 }
